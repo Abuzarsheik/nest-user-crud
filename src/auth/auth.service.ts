ad9b46157c944cd193eb715c424d521a4ff9ef65
Abuzarzubair <abuzarzubair15@gmail.com>
2025-09-07 11:21:53 -0700

    completed signup logic + handle error logic+ improved prisma schema

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async register(dto: AuthDTO) {
-    console.log(Object.keys(this.prisma));
-
     // generate hash password
     const hash = await argon.hash(dto.password);
 
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+
+        // only return this
+        select: {
+          email: true,
+          id: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        }
+      }
+      throw error;
+    }
+
     // save new user in db
-
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-
-    return user;
   }
   login() {
     return 'sing in confirmed';
